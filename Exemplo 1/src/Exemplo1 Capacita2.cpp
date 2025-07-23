feb75ecb308f114c460f9e8fc7d873988e50c0cd
Dionysio Rangel <dionysio.20241@poli.ufrj.br>
2025-07-23 01:08:01 -0300

    Commit 2, dois exemplos prontos

---
+++ Exemplo 1/src/Exemplo1 Capacita2.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+// Condicional
+if (idade >= 18) {
+    print("É maior de idade.");
+} else {
+    print("É menor de idade.");
+}
+
+// Laço
+for (i = 0; i < 5; i++) {
+    print(i); // Imprime 0, 1, 2, 3, 4
+}
+
+// Lista de números
+numeros = [1, 2, 3, 4, 5];
+
+// Dicionário de informações de uma pessoa
+pessoa = {
+    "nome": "Carlos",
+    "cidade": "São Paulo",
+    "idade": 40
+};
